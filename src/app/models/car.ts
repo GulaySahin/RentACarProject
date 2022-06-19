ad72e0b4ca8400f3d2e30dc76c7bca4b49edd429
ETIYACORP\gulay.sahin <gulay.sahin@etiya.com>
2022-06-19 11:44:50 +0300

    commit-1

---
+++ src/app/models/car.ts
@@ -0,0 +1,12 @@
+export interface Car {
+  id:number,
+  carName:string,
+  city:string,
+  dailyPrice:number,
+  description:string,
+  findexPoint:number,
+  kilometer:number,
+  modelYear:number,
+  brandId:number,
+  colorId:number
+}
